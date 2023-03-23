51cad995a5049f18e9468cc4db9b278a413a2415
adrian <xwrobel02@gmail.com>
2023-03-23 03:11:54 +0100

    login changes

--- src/features/users/Register.jsx
+++ src/features/users/Register.jsx
@@ -97,11 +97,11 @@ const Register = () => {
                 <button type='button' className="login-form-button" onClick={() => window.location.href = url}>Continue with Google</button>
                 <span className="login-form-divider"><div className="line-through-div"></div>or<div className="line-through-div"></div></span>
                 <label htmlFor='email' className='label-hidden'>Username</label>
-                <input id="email" type="text" placeholder="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input id="email" type="text" placeholder="Username" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label htmlFor='password' className='label-hidden'>Password</label>
-                <input id="password" type='password' placeholder="password" name="password" value={password} autoComplete="new-password" onChange={(e) => setPassword(e.target.value)} />
+                <input id="password" type='password' placeholder="Password" name="password" value={password} autoComplete="new-password" onChange={(e) => setPassword(e.target.value)} />
                 <label htmlFor='nickname' className='label-hidden'>Nickname</label>
-                <input id="nickname" type="text" placeholder="display name" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input id="nickname" type="text" placeholder="Nickname" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 <button type='submit' className="login-form-button" disabled={!canRegister}
                 >Sign up</button>
             </form>
